c453e571515b0982b6bca251b6de87c6abf414fb
calogerosorce <calogerosorce00@hotmail.com>
2025-10-22 13:45:04 +0200

    Add logic to increment numbers and log results

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,16 @@
 const numbers = [2, 8, 4, 7, 2, 87];
+const addNum = []
+for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    ++element
+    addNum.push(element)
+}
+
+console.log(addNum);
+
+const addOneNum = numbers.map(element => ++element)
+console.log(addOneNum);
+
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
