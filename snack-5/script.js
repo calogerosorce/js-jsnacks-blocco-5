00b63fd8becf7c2c82da495fe09b2f1f2d71bb66
calogerosorce <calogerosorce00@hotmail.com>
2025-10-22 13:49:11 +0200

    Add logic to filter and log even numbers from the array

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,17 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const even = []
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    if (element % 2 === 0) {
+        even.push(element)
+    }
+}
+console.log(even);
+
+const pari = nums.filter(element => element % 2 === 0)
+console.log(pari);
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
